54eddfdd23d1a203a6f41c99379e9ff61bdf68bd
tasnim_elgarsi <tasnimgarsi@gmail.com>
2023-10-10 21:47:10 +0100

    first commit

--- src/app/freelancer.guard.ts
+++ src/app/freelancer.guard.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+/*import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FreelancerGuard implements CanActivate {
+export class FreelancerGuard  {
   constructor (private authService: AuthService,
     private router : Router) {}
     
@@ -23,4 +23,32 @@ export class FreelancerGuard implements CanActivate {
     
   
 }
+}*/
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from './services/auth.service';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+ 
+class PermissionsService  {
+ 
+  constructor (private authService: AuthService,
+    private router : Router) {}
+ 
+ 
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (this.authService.isAdmin())
+    return true;
+  else {
+    this.router.navigate(['app-forbidden']);
+    return false;
+  }
+  }
+ 
+}
+ 
+export const FreelancerGuard: CanActivateFn = (next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean => {
+  return inject(PermissionsService ).canActivate(next, state);
 }
